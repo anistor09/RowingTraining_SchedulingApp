de08b13369966f472ebc4b08eb9b9d12c529d057
Alexandru Nistor <a.nistor@student.tudelft.nl>
2022-12-08 17:57:41 +0100

    Add Controllers to userMicroservice

---
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/DefaultController.java
@@ -0,0 +1,87 @@
+package nl.tudelft.sem.template.example.controllers;
+
+import nl.tudelft.sem.template.example.authentication.AuthManager;
+import nl.tudelft.sem.template.example.domain.models.ParticipationRequestModel;
+import nl.tudelft.sem.template.example.domain.models.RequestMatchModel;
+import nl.tudelft.sem.template.example.domain.participant.Certificate;
+import nl.tudelft.sem.template.example.domain.participant.ParticipantService;
+import nl.tudelft.sem.template.example.domain.participant.PositionManager;
+import nl.tudelft.sem.template.example.domain.participant.NetId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+/**
+ * Hello World example controller.
+ * <p>
+ * This controller shows how you can extract information from the JWT token.
+ * </p>
+ */
+@RestController
+public class DefaultController {
+
+    private final transient AuthManager authManager;
+    private final transient ParticipantService participantService;
+
+    /**
+     * Instantiates a new controller.
+     *
+     * @param authManager Spring Security component used to authenticate and authorize the user
+     */
+    @Autowired
+    public DefaultController(AuthManager authManager, ParticipantService participantService) {
+        this.authManager = authManager;
+        this.participantService= participantService;
+    }
+
+    /**
+     * Gets example by id.
+     *
+     * @return the example found in the database with the given id
+     */
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello " + authManager.getNetId());
+
+    }
+
+    /**
+     * Endpoint for adding a new participant
+     *
+     * @param request The registration model
+     * @return 200 OK if the registration is successful
+     * @throws Exception
+     */
+    @PostMapping("/details")
+    public ResponseEntity addDetails(@RequestBody ParticipationRequestModel request){
+        try{
+            NetId netId= new NetId(authManager.getNetId());
+            PositionManager positionManager= new PositionManager(request.getPositions());
+            Certificate certificate= new Certificate(request.getCertificate());
+            String gender= request.getGender();
+            //here we need to throw an exception if the entered certificate is wrong
+            String organization= request.getOrganization();
+            String level= request.getLevel();
+            participantService.addParticipant(netId,positionManager,gender,certificate,organization,level);
+        }   catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        return ResponseEntity.ok("Fine");
+    }
+    @PostMapping("/requestMatch")
+    public ResponseEntity requestMatch(@RequestBody RequestMatchModel request) {
+        List<String> timeSlots = request.getTimeslots();
+        NetId netId= new NetId(authManager.getNetId());
+        participantService.requestMatch(netId,timeSlots);
+
+        return ResponseEntity.ok().build();
+
+    }
+}
---
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/UserDetailsController.java
@@ -0,0 +1,95 @@
+package nl.tudelft.sem.template.example.controllers;
+
+import nl.tudelft.sem.template.example.authentication.AuthManager;
+import nl.tudelft.sem.template.example.domain.participant.ParticipantService;
+import nl.tudelft.sem.template.example.domain.participant.NetId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("userDetails")
+public class UserDetailsController {
+    private final transient AuthManager authManager;
+    private final transient ParticipantService participantService;
+    /**
+     * Instantiates a new controller.
+     *
+     * @param authManager Spring Security component used to authenticate and authorize the user
+     */
+    @Autowired
+    public UserDetailsController(AuthManager authManager, ParticipantService participantService) {
+        this.authManager = authManager;
+        this.participantService= participantService;
+    }
+    @GetMapping("/positions")
+    public List<String> getPositions() {
+        List<String> positions;
+        try {
+            NetId participantName = new NetId(authManager.getNetId());
+             positions = participantService.getParticipantPositions(participantName);
+
+        }   catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        return positions;
+
+    }
+
+    @GetMapping("/certificate")
+    public String getCertificate() {
+        String certificate="";
+        try {
+            NetId participantName = new NetId(authManager.getNetId());
+            certificate = participantService.getParticipantCertificate(participantName);
+
+        }   catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        return certificate;
+
+    }
+    @GetMapping("/gender")
+    public String getGender() {
+        String gender="";
+        try {
+            NetId participantName = new NetId(authManager.getNetId());
+            gender = participantService.getParticipantGender(participantName);
+
+        }   catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        return gender;
+
+    }
+    @GetMapping("/level")
+    public String getLevel() {
+        String level="";
+        try {
+            NetId participantName = new NetId(authManager.getNetId());
+            level = participantService.getParticipantLevel(participantName);
+
+        }   catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        return level;
+
+    }
+    @GetMapping("/organization")
+    public String getOrganization() {
+        String organization="";
+        try {
+            NetId participantName = new NetId(authManager.getNetId());
+            organization = participantService.getParticipantOrganization(participantName);
+
+        }   catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        return organization;
+    }
+}
