852f7ebf3ba37fdea0d95cf23478602e8332d285
Kyungmin Nam <K.Nam-2@student.tudelft.nl>
2022-12-08 17:43:43 +0100

    Add ActivityRepository and ActivityService

---
+++ activity-microservice/src/main/java/nl/tudelft/sem/template/example/domain/ActivityService.java
@@ -0,0 +1,59 @@
+package nl.tudelft.sem.template.example.domain;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ActivityService {
+    private final ActivityRepository activityRepository;
+
+    public ActivityService(ActivityRepository activityRepository) {
+        this.activityRepository = activityRepository;
+    }
+
+    public List<Activity> getAll() {
+        return activityRepository.findAll();
+    }
+
+    public List<Training> getTrainings() {
+        List<Activity> activities = getAll();
+        List<Training> trainings = new ArrayList<>();
+        for(Activity activity : activities) {
+            if(activity instanceof Training)
+                trainings.add((Training) activity);
+        }
+        return trainings;
+    }
+
+    public List<Competition> getCompetitions() {
+        List<Activity> activities = getAll();
+        List<Competition> competitions = new ArrayList<>();
+        for(Activity activity : activities) {
+            if(activity instanceof Competition)
+                competitions.add((Competition) activity);
+        }
+        return competitions;
+    }
+
+    public List<Activity> getByUsername(String username) {
+        List<Activity> activities = getAll();
+        List<Activity> result = new ArrayList<>();
+        for(Activity activity : activities) {
+            if(activity.getOwner().toString().equals(username))
+                result.add(activity);
+        }
+        return result;
+    }
+
+    public Training createTraining(Username username, ActivityRequestModel request) {
+        username = new Username("user");
+        Training training = new Training(username, request.getDateTime(), request.getBoat(), request.getPositions());
+        //System.out.println(training.toString());
+        activityRepository.save(training);
+        return training;
+    }
+
+}
