cf773889e931d4bb7c66a300fee61462f56f0602
Kyungmin Nam <K.Nam-2@student.tudelft.nl>
2022-12-08 17:43:02 +0100

    Add Username class and its Converter

---
+++ activity-microservice/src/main/java/nl/tudelft/sem/template/example/domain/UsernameAttributeConverter.java
@@ -0,0 +1,15 @@
+package nl.tudelft.sem.template.example.domain;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class UsernameAttributeConverter implements AttributeConverter<Username, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Username username){return username.toString();}
+
+    @Override
+    public Username convertToEntityAttribute(String dbData){return new Username(dbData);}
+
+}
