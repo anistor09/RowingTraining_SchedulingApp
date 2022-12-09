83b55d12722f9e3008d004b02a72cce85d9f76cc
Paulina Sobocińska <P.K.Sobocinska@student.tudelft.nl>
2022-12-09 21:52:09 +0100

    very small change

--- activity-microservice/src/main/java/nl/tudelft/sem/template/example/domain/Training.java
+++ activity-microservice/src/main/java/nl/tudelft/sem/template/example/domain/Training.java
@@ -3,7 +3,6 @@ package nl.tudelft.sem.template.example.domain;
 import javax.persistence.Entity;
 import java.sql.Time;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
