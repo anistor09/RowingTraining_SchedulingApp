cf0360d6e6225a6bca1d128d55fc3290468b16af
Rareș Mihail <R.Mihail@student.tudelft.nl>
2022-12-21 12:58:49 +0100

    NetId validation in authentication microservice

--- authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/domain/user/NetId.java
+++ authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/domain/user/NetId.java
@@ -2,6 +2,8 @@ package nl.tudelft.sem.template.authentication.domain.user;
 
 import lombok.EqualsAndHashCode;
 
+import java.util.regex.Pattern;
+
 /**
  * A DDD value object representing a NetID in our domain.
  */
@@ -9,9 +11,15 @@ import lombok.EqualsAndHashCode;
 public class NetId {
     private final transient String netIdValue;
 
+    private final transient Pattern pattern = Pattern.compile("[a-zA-Z0-9_]");
+
     public NetId(String netId) {
         // validate NetID
         this.netIdValue = netId;
+    }
+
+    public boolean isValid(){
+        return (pattern.matcher(netIdValue).matches()) && (netIdValue.length() < 30) && (netIdValue != null);
     }
 
     @Override
