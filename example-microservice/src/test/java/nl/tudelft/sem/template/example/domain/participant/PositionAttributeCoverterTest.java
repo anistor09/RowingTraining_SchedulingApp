edb77005b42d68d496812533980c6a5eb80d211a
alexandrumarin <alexandrumarin@student.tudelft.nl>
2022-12-22 23:10:56 +0100

    More tests for participant microservice

--- example-microservice/src/test/java/nl/tudelft/sem/template/example/domain/participant/PositionAttributeCoverterTest.java
+++ example-microservice/src/test/java/nl/tudelft/sem/template/example/domain/participant/PositionAttributeCoverterTest.java
@@ -25,5 +25,15 @@ class PositionAttributeCoverterTest {
 
     @Test
     void convertToEntityAttribute() {
+        PositionAttributeCoverter pac= new PositionAttributeCoverter();
+        List<String> pos = Arrays.asList("cox", "coach", "port side rower", "starboard side rower", "sculling rower");
+        int min=0;
+        int max=4;
+        int random1 = (int)Math.floor(Math.random()*(max-min+1)+min);
+        int random2 = (int)Math.floor(Math.random()*(max-min+1)+min);
+        String positions=pos.get(random1)+","+pos.get(random2);
+        PositionManager pm = new PositionManager(positions);
+        assertTrue(pac.convertToEntityAttribute(positions).equals(pm));
+
     }
 }
