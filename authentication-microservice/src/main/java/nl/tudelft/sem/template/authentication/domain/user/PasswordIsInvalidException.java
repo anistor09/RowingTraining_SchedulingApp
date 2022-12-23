b0f516ad392fbb605ece7022957568a0818c08c7
Paulina Sobocińska <P.K.Sobocinska@student.tudelft.nl>
2022-12-23 02:44:31 +0100

    fix pmd

--- authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/domain/user/PasswordIsInvalidException.java
+++ authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/domain/user/PasswordIsInvalidException.java
@@ -1,6 +1,7 @@
 package nl.tudelft.sem.template.authentication.domain.user;
 
 public class PasswordIsInvalidException extends Exception {
+    private serialVersionUID = 1L;
     public PasswordIsInvalidException() {
         super("Password is invalid.");
     }
