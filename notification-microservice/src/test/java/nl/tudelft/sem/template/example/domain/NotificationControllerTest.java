789555a9f6fd2b5c2a4f5a74df905f04498e1bad
Paulina Sobocińska <P.K.Sobocinska@student.tudelft.nl>
2022-12-23 14:01:40 +0100

    setting up the integration test?

--- notification-microservice/src/test/java/nl/tudelft/sem/template/example/domain/NotificationControllerTest.java
+++ notification-microservice/src/test/java/nl/tudelft/sem/template/example/domain/NotificationControllerTest.java
@@ -1,27 +1,47 @@
 package nl.tudelft.sem.template.example.domain;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.tudelft.sem.template.example.authentication.AuthManager;
+import nl.tudelft.sem.template.example.authentication.JwtTokenVerifier;
 import nl.tudelft.sem.template.example.controllers.NotificationController;
 import nl.tudelft.sem.template.example.domain.models.NotificationRequestModel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 public class NotificationControllerTest {
-    @Mock
-    private NotificationService notificationService;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @Mock
-    private AuthManager authManager;
+    @Autowired
+    private transient JwtTokenVerifier mockJwtTokenVerifier;
 
-    @Test
-    public void createParticipantNotificationSuccessful() {
-        NotificationController notificationController = new NotificationController(notificationService, authManager);
-        //NOT FINISHED
+    @Autowired
+    private transient AuthManager mockAuthenticationManager;
+
+    @Autowired
+    private transient NotificationRepository notifiactionRepository;
+
+    @BeforeEach
+    void setUp() {
+        when(mockAuthenticationManager.getNetId()).thenReturn("ExampleUser");
+        when(mockJwtTokenVerifier.validateToken(anyString())).thenReturn(true);
+        when(mockJwtTokenVerifier.getNetIdFromToken(anyString())).thenReturn("ExampleUser");
     }
+
+
 }
