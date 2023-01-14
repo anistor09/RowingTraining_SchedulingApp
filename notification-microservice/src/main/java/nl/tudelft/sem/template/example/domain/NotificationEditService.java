cde54a681f195f0c013e1bb0be16f462df2572f2
Rareș Mihail <R.Mihail@student.tudelft.nl>
2023-01-14 14:45:30 +0100

    split NotificationService into 2 classes

--- notification-microservice/src/main/java/nl/tudelft/sem/template/example/domain/NotificationEditService.java
+++ notification-microservice/src/main/java/nl/tudelft/sem/template/example/domain/NotificationEditService.java
@@ -1,19 +1,18 @@
 package nl.tudelft.sem.template.example.domain;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-public class NotificationService {
+public class NotificationEditService {
     private final transient NotificationRepository notificationRepository;
 
     /**
      * Constructor for a notification service
      * @param notificationRepository
      */
-    public NotificationService(NotificationRepository notificationRepository){
+    public NotificationEditService(NotificationRepository notificationRepository){
         this.notificationRepository = notificationRepository;
     }
 
@@ -40,31 +39,6 @@ public class NotificationService {
         return notificationRepository.save(notification);
     }
 
-    /**
-     * Gets all notifications.
-     * @return
-     */
-    public List<Notification> getAllNotifications(){
-        return notificationRepository.findAll();
-    }
-
-    /**
-     * Gets all notifications for a specific user.
-     * @param netId
-     * @return
-     */
-    public List<Notification> getUserNotifications(NetId netId){
-        return notificationRepository.getAllByNetId(netId);
-    }
-
-    /**
-     * Gets all notifications for a specific user.
-     * @param ownerId
-     * @return
-     */
-    public List<Notification> getOwnerNotifications(NetId ownerId){
-        return notificationRepository.getAllByOwnerId(ownerId);
-    }
 
     /**
      * Deletes a notification.
