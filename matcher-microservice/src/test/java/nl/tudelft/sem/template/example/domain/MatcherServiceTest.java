ff5e24e5c833e7ba2f6fac3fcd7d4e82acdd04fb
Kyungmin Nam <K.Nam-2@student.tudelft.nl>
2023-01-22 01:29:03 +0100

    add tests for MatcherEditService
    kills mutant

--- matcher-microservice/src/test/java/nl/tudelft/sem/template/example/domain/MatcherServiceTest.java
+++ matcher-microservice/src/test/java/nl/tudelft/sem/template/example/domain/MatcherServiceTest.java
@@ -1,30 +1,39 @@
 package nl.tudelft.sem.template.example.domain;
 
 import nl.tudelft.sem.template.example.domain.transferObject.RequestMatch;
+import nl.tudelft.sem.template.example.domain.transferObject.TransferMatch;
+import nl.tudelft.sem.template.example.domain.utils.ServerUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 class MatcherServiceTest {
-//    transient MatcherService matcherService;
-//    @BeforeEach
-//    void setUp(){
-//        matcherService = new MatcherService();
-//    }
+    transient MatcherEditService matcherEditService;
+    MatcherRepository matcherRepository;
 
-    @Test
-    void computeMatch() {
+    @BeforeEach
+    void setUp(){
+        matcherRepository = Mockito.mock(MatcherRepository.class);
+        matcherEditService = new MatcherEditService(matcherRepository, new ServerUtils());
+    }
+
+//    @Test
+//    void computeMatch() {
 //        Participant participant = new Participant(new NetId("netId"),
 //                new PositionManager("cox"),
 //                "M",
 //                new Certificate("C4"),
 //                "org",true);
 //        RequestMatch requestMatch = new RequestMatch(participant, List.of("21-12-2012 17:33;29-12-2022 15:22"));
-
-    }
+//
+//    }
 
     @Test
     void getActivities() {
@@ -48,10 +57,20 @@ class MatcherServiceTest {
 
     @Test
     void removeMatches() {
+        TransferMatch tm = new TransferMatch(1L, "cox", "20-12-2022 09:00;20-12-2022 11:00",
+                "participant", "owner");
+        Match match = new Match("participant", 1L, "coach");
+        Mockito.when(matcherRepository.findAll()).thenReturn(List.of(match));
+        matcherEditService.removeMatches(List.of(tm));
+        verify(matcherRepository, times(1)).delete(match);
     }
-
     @Test
     void findMatch() {
+        TransferMatch tm = new TransferMatch(1L, "cox", "20-12-2022 09:00;20-12-2022 11:00",
+                "participant", "owner");
+        Match match = new Match("participant", 1L, "coach");
+        List<Match> matches = matcherEditService.findMatch(tm, List.of(match));
+        assertThat(matches).containsExactly(match);
     }
 
     @Test
